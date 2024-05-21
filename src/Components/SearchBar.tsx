0348358affe8b4ce1b2c70ffc66f4f33c7c14687
mrprabhat010 <mrprabhat010gmail.com>
2024-05-21 09:08:07 +0530

    url sync in searchBar

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,11 +1,21 @@
+import React, {useEffect} from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { useSearchStore } from '../store/searchStore';
 
 const SearchBar = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const { query, setQuery } = useSearchStore();
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
+    const newQuery = e.target.value;
+    setQuery(newQuery);
+    setSearchParams({ query: newQuery });
   };
+
+useEffect(() => {
+    const initialQuery = searchParams.get('query') || '';
+    setQuery(initialQuery);
+  }, [searchParams, setQuery]);
 
   return (
     <input
