98187c5fd59fd7fe0e317821a01f81badbd24d43
mrprabhat010 <mrprabhat010gmail.com>
2024-05-21 09:05:10 +0530

    added pages

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import SearchBar from '../components/SearchBar';
+import SearchResults from '../components/SearchResults';
+
+const Home = () => (
+  <div>
+    <SearchBar />
+    <SearchResults />
+  </div>
+);
+
+export default Home;
---
+++ src/Pages/Post.tsx
@@ -0,0 +1,9 @@
+import PostDetail from '../components/PostDetail';
+
+const Post = () => (
+  <div>
+    <PostDetail />
+  </div>
+);
+
+export default Post;
